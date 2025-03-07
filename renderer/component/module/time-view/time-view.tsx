8ae410fd6a1312ba01a84ec2f1c0ea5d717a0d22
Ziphil <yfe23457@nifty.com>
2025-03-07 18:55:49 +0900

    変更

--- renderer/component/module/time-view/time-view.tsx
+++ renderer/component/module/time-view/time-view.tsx
@@ -17,14 +17,14 @@ export const TimeView = create(
     const hour = Math.floor(time / 1000 / 60 / 60);
     const minute = Math.floor(time / 1000 / 60) % 60;
     const hourString = hour.toString();
-    const minuteString = (hour === 0) ? minute.toString() : minute.toString().padStart(2, "0");
+    const minuteString = minute.toString().padStart(2, "0");
 
     return (
       <span styleName="root" className={className}>
-        {(hour > 0) && [...hourString].map((digit, index) => (
+        {[...hourString].map((digit, index) => (
           <span key={index} styleName="digit">{digit}</span>
         ))}
-        {(hour > 0) && (
+        {(
           <span styleName="colon">:</span>
         )}
         {[...minuteString].map((digit, index) => (
